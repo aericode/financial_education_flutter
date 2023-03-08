e755132706438567a51ce3be55f7c32d92979422
aericode <e.andrade5f@gmail.com>
2023-03-08 17:55:59 -0300

    basic functionalities implemented

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-
 void main() async {
   runApp(const MyApp());
 }
 
-const request = "https://api.hgbrasil.com/finance?format=json-cors&key=72697c9d";
+const request =
+    "https://api.hgbrasil.com/finance?format=json-cors&key=72697c9d";
 
 Future<Map> getData() async {
   http.Response response = await http.get(Uri.parse(request));
@@ -59,18 +59,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  double ibovpoints = 0;
+  double euro = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -85,110 +75,136 @@ class _MyHomePageState extends State<MyHomePage> {
           centerTitle: true,
           backgroundColor: Colors.green.shade900),
       body: DefaultTextStyle(
-        style: Theme
-            .of(context)
-            .textTheme
-            .bodyMedium!,
+        style: Theme.of(context).textTheme.bodyMedium!,
         child: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints viewportConstraints) {
             return SingleChildScrollView(
-                child: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      minHeight: viewportConstraints.maxHeight,
-                    ),
-                    child: IntrinsicHeight(
-                        child: Column(
-                            children: <Widget>[
-                        Padding(
-                        padding: EdgeInsets.all(25.0),
-                        child: Image.asset('assets/bovespa.jpg', width: 300)),
-                    Container(
-                      // A fixed-height child.
-                      alignment: Alignment.topCenter,
-                      child: const Text(
-                          'Conhecido como IBOV, o índice funciona como um termômetro do mercado acionário do Brasil',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 25)),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.symmetric(vertical: 15.0),
-                        child:
-                        Image.asset('assets/construir.jpeg', width: 340)),
-                    Container(
-                      // A fixed-height child.
-                      alignment: Alignment.topCenter,
-                      child: const Text(
-                          'Para medir este índice, são observados o valor de 92 ações de alta negociação na bolsa',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 25)),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.symmetric(vertical: 15.0),
-                        child:
-                        Image.asset('assets/dinheiro.jpg', width: 340)),
-                    Container(
-                      // A fixed-height child.
-                      alignment: Alignment.topCenter,
-                      child: const Text(
-                          'Esse conjunto de ações corresponde a cerca de 80% de todo o volume negociado na bolsa de valores brasileira',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 25)),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.symmetric(vertical: 15.0),
-                        child: Image.asset('assets/multiplica.jpeg',
-                            width: 340)),
-                    Container(
-                      // A fixed-height child.
-                      alignment: Alignment.topCenter,
-                      child: const Text(
-                          'Algumas ações tem influência maior do que outras nesse termômetro então cada uma é multiplicada por um "peso" e todas tem seu valor somado',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 25)),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.symmetric(vertical: 15.0),
-                        child: Image.asset('assets/grafico.png', width: 400)),
-                    Container(
-                      // A fixed-height child.
-                      alignment: Alignment.topCenter,
-                      child: const Text(
-                          'Isso permite gerar um número e acompanhar em tempo real, a valorização ou desvalorização desse conjunto de ações, que diz bastante a respeito do nossso mercado',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 25)),
-                    ),
-                    Column(children: <Widget>[
-                    Container(
-                    // A fixed-height child.
-                      padding: EdgeInsets.fromLTRB(0,100,0,10),
-                    alignment: Alignment.center,
-                        child: Text('No momento o índice IBOVESPA está com',
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                ),
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                          padding: EdgeInsets.all(25.0),
+                          child: Image.asset('assets/bovespa.jpg', width: 300)),
+                      Container(
+                        // A fixed-height child.
+                        alignment: Alignment.topCenter,
+                        child: const Text(
+                            'Conhecido como IBOV, o índice funciona como um termômetro do mercado acionário do Brasil',
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 30))),
-                    Text('106.039,00',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 50,
-                            color: Colors.blue)),
-                Text('pontos',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30))
+                                fontWeight: FontWeight.bold, fontSize: 25)),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(vertical: 15.0),
+                          child:
+                              Image.asset('assets/construir.jpeg', width: 340)),
+                      Container(
+                        // A fixed-height child.
+                        alignment: Alignment.topCenter,
+                        child: const Text(
+                            'Para medir este índice, são observados o valor de 92 ações de alta negociação na bolsa',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 25)),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(vertical: 15.0),
+                          child:
+                              Image.asset('assets/dinheiro.jpg', width: 340)),
+                      Container(
+                        // A fixed-height child.
+                        alignment: Alignment.topCenter,
+                        child: const Text(
+                            'Esse conjunto de ações corresponde a cerca de 80% de todo o volume negociado na bolsa de valores brasileira',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 25)),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(vertical: 15.0),
+                          child: Image.asset('assets/multiplica.jpeg',
+                              width: 340)),
+                      Container(
+                        // A fixed-height child.
+                        alignment: Alignment.topCenter,
+                        child: const Text(
+                            'Algumas ações tem influência maior do que outras nesse termômetro então cada uma é multiplicada por um "peso" e todas tem seu valor somado',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 25)),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(vertical: 15.0),
+                          child: Image.asset('assets/grafico.png', width: 400)),
+                      Container(
+                        // A fixed-height child.
+                        alignment: Alignment.topCenter,
+                        child: const Text(
+                            'Isso permite gerar um número e acompanhar em tempo real, a valorização ou desvalorização desse conjunto de ações, e extrair a partir dessa amostra informações sobre nossso mercado',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 25)),
+                      ),
+                      Column(children: <Widget>[
+                        Container(
+                            // A fixed-height child.
+                            padding: EdgeInsets.fromLTRB(0, 180, 0, 10),
+                            alignment: Alignment.center,
+                            child: Text('No momento o índice IBOVESPA está em',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30))),
+                        FutureBuilder<Map>(
+                            future: getData(),
+                            builder: (context, snapshot) {
+                              switch (snapshot.connectionState) {
+                                case ConnectionState.none:
+                                case ConnectionState.active:
+                                case ConnectionState.waiting:
+                                  return const Center(
+                                      child: Text(
+                                    "Carregando dados...",
+                                    style: TextStyle(
+                                        color: Colors.amber, fontSize: 25.0),
+                                    textAlign: TextAlign.center,
+                                  ));
+                                default:
+                                  if (snapshot.hasError) {
+                                    return const Center(
+                                        child: Text(
+                                      "Erro ao carregar dados...",
+                                      style: TextStyle(
+                                          color: Colors.amber, fontSize: 25.0),
+                                      textAlign: TextAlign.center,
+                                    ));
+                                  } else {
+                                    ibovpoints = snapshot.data!["results"]
+                                        ["stocks"]["IBOVESPA"]["points"];
 
-                ]),
-            ],
-            ),
-            ),
-            ),
+                                    return SingleChildScrollView(
+                                        child: Text(ibovpoints.toString(),
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 50,
+                                                color: Colors.blue)));
+                                  }
+                              }
+                            }),
+                        Text('pontos',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 30))
+                      ]),
+                    ],
+                  ),
+                ),
+              ),
             );
           },
         ),
